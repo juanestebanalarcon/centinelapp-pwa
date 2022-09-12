ba7d78f7ef6ab4276d60de9756ca75154b7aa035
Diana Marcela Garcia Lopez <diana_ma.garcia@uao.edu.co>
2022-09-11 20:20:42 -0500

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,34 @@
 
 import '../styles/navbar.css'
+import { useNavigate } from 'react-router-dom';
 export function Navbar(){
+    const navigate = useNavigate();
+    
+    function home(e){ 
+        e.preventDefault();
+        navigate(`/home`)
+    }
     return(
         <div className='App'>
     
         <div className="Navbar">
         <div className='Iconos-Navbar'>
             <div className="home" href="/home">
-                
-               <img src='./images/navbar/home.png'/>
+               <button onClick={home}> 
+               <img src='./images/navbar/home.png' alt='home'/>
+               </button>
             </div>
             <div className="messages">
                 
-                <img src='./images/navbar/messages.png'/>
+                <img src='./images/navbar/messages.png' alt='mensaje'/>
             </div>
             <div className="calendar">
                 
-                <img src='./images/navbar/calendar.png'/>
+                <img src='./images/navbar/calendar.png' alt='calendario'/>
             </div>
             <div className="person">
                 
-                <img src='./images/navbar/person.png'/>
+                <img src='./images/navbar/person.png' alt='persona'/>
             </div>
             </div>
             
