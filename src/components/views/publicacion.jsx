455f00805d33f96e10878ff99d62815c4bd9ed5e
Diana Marcela Garcia Lopez <diana_ma.garcia@uao.edu.co>
2022-09-16 14:37:43 -0500

    css

--- src/components/views/publicacion.jsx
+++ src/components/views/publicacion.jsx
@@ -15,7 +15,7 @@ export const PublicacionGeneral= ()=>{
         <Header/>
         <div className="conte-imp">
         <h1>Publicaciones</h1>
-        <h2>Selecciona una rama para ver sus mensajes, en icono + púedes crear una nueva publicacion</h2>
+        <h3>Selecciona una rama para ver sus mensajes, en icono + púedes crear una nueva publicacion</h3>
         <SelectCreacion nombre="General" desc="Publicaciones para todos"/>
         <SelectCreacion nombre="Cachorros" desc="10-12 Años"/>
         </div>
